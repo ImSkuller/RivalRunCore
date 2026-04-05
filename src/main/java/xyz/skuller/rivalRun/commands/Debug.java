d0ccc42794905eafe0c2b5edd1614dab252874cc
Skuller <hrishikeshrs321@gmail.com>
2026-04-05 18:57:57 +0530

    Debug Command Impl for mode switching

--- src/main/java/xyz/skuller/rivalRun/commands/Debug.java
+++ src/main/java/xyz/skuller/rivalRun/commands/Debug.java
@@ -6,23 +6,45 @@ import org.bukkit.command.TabExecutor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import xyz.skuller.rivalRun.RivalRun;
+import xyz.skuller.rivalRun.managers.GameStateManager;
 
 import java.util.List;
 
 public class Debug implements TabExecutor {
 
-    Boolean debugEnabled = RivalRun.getInstance().getConfig().getBoolean("commands.debug");
-
     @Override
-    public boolean onCommand(@NotNull CommandSender commandSender,
+    public boolean onCommand(@NotNull CommandSender sender,
                              @NotNull Command command,
                              @NotNull String s,
-                             @NotNull String @NotNull [] strings)
+                             @NotNull String @NotNull [] args)
     {
 
+        boolean debugEnabled = RivalRun.getInstance().getConfig().getBoolean("commands.debug");
+        if (!debugEnabled) {
+            sender.sendRichMessage("<red>Debug command is not enabled");
+            return true;
+        }
 
-        if (!debugEnabled) { // Remove the exclamation mark before release
-            commandSender.sendRichMessage("<red>Debug command is not enabled");
+        GameStateManager gameStateManager = RivalRun.getInstance().getGameStateManager();
+
+        if (args[0].equalsIgnoreCase("MODE")) {
+
+            GameStateManager.GameStates newState;
+
+            try {
+                newState = GameStateManager.GameStates.valueOf(args[1].toUpperCase());
+            } catch (IllegalArgumentException e) {
+                sender.sendRichMessage("<red>Invalid game state!");
+                return true;
+            }
+
+            if (gameStateManager.isState(newState)) {
+                sender.sendRichMessage("<red>The game state is already " + newState);
+                return true;
+            }
+
+            gameStateManager.setState(newState);
+            sender.sendRichMessage("<green>Game state has been set to " + newState + ", You may now test that state.");
             return true;
         }
 
@@ -31,11 +53,21 @@ public class Debug implements TabExecutor {
     }
 
     @Override
-    public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender,
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender,
                                                 @NotNull Command command,
                                                 @NotNull String s,
-                                                @NotNull String @NotNull [] strings)
+                                                @NotNull String @NotNull [] args)
     {
+        if (args.length == 1) {
+            return List.of("mode");
+        }
+
+        if (args.length == 2 && args[0].equalsIgnoreCase("mode")) {
+            return java.util.Arrays.stream(GameStateManager.GameStates.values())
+                    .map(Enum::name)
+                    .toList();
+        }
+
         return List.of();
     }
 }
